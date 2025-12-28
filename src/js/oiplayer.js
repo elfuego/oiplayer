9781984763a5beb192dc24a622149a4396c5fbaf
André van Toly <andre@toly.nl>
2025-12-28 16:09:59 +0100

    WIP with js constants

--- src/js/oiplayer.js
+++ src/js/oiplayer.js
@@ -1,3 +1,5 @@
+import { SVG_FULLSCREEN, SVG_PAUSE, SVG_PLAY, SVG_VOLUME, SVG_VOLUME_MUTED } from "./constants";
+
 class Player {
   constructor(media, oiplayer, config) {
     this.media = media;
@@ -464,29 +466,35 @@ class OIPlayer {
   makeControlsHtml() {
     const sec = this.mediaDuration ? this.mediaDuration : 0;
 
-    const html = `<ul class="controls">
+    const html = `<ul class="controls" aria-description="Media controls">
         <li class="play">
-          <button data-button-play="none"><span>Play</span></button>
+          <button data-button-play="play" aria-label="Play/Pause">
+            <span data-button-play-icon="play">${SVG_PLAY}</span>
+            <span data-button-play-icon="paused">${SVG_PAUSE}</span>
+          </button>
         </li>
         <li class="timeleft">
-          <div data-timeleft="">00:00</div>
+          <div data-timeleft="" aria-label="Time left">00:00</div>
         </li>
         <li class="progress">
           <div class="bar push" data-progress="push">
             <button class="pos"><span>Position</span></button>
           </div>
-          <div data-progress="played" class="bar played"></div>
-          <div data-progress="loaded" class="bar loaded"></div>
+          <div data-progress="played" class="bar played" aria-label="Played"></div>
+          <div data-progress="loaded" class="bar loaded" aria-label="Loaded"></div>
           <div data-progress="back" class="bar back"></div>
         </li>
-        <li class="time">
+        <li class="time" aria-label="Time">
           <div data-time="">${this._totime(sec)}</div>
         </li>
         <li class="screen">
-          <button data-button-screen><span>Screen</span></button>
+          <button data-button-screen aria-label="Fullscreen">${SVG_FULLSCREEN}</button>
         </li>
         <li class="sound">
-          <button data-button-volume=""><span>Volume</span></button>
+          <button data-button-volume="on" aria-label="Mute/Unmute">
+            <span data-button-volume-icon="on">${SVG_VOLUME}</span>
+            <span data-button-volume-icon="muted">${SVG_VOLUME_MUTED}</span>
+          </button>
         </li>
       </ul>`;
 
